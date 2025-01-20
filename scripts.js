19ca72cecb76954535922188c68745b6e3d15296
oliver F <151986513+Forest-Fr@users.noreply.github.com>
2025-01-20 17:04:18 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
  * scripts.js
  * 
  * 1. Hero轮播：全屏 100vh (PC端)，自动切换
@@ -12,14 +12,10 @@ if (heroSlides) {
   let currentIndex = 0;
   const totalSlides = heroSlides.children.length;
 
-  // 确保图片容器 hero-slides 的宽度可以适应所有子元素
-  heroSlides.style.width = `${100 * totalSlides}%`; // 容器宽度设置为图片数量的 100%
-
-  // 自动切换
   setInterval(() => {
-    currentIndex = (currentIndex + 1) % totalSlides;  // 循环切换
-    heroSlides.style.transform = `translateX(-${currentIndex * 100}%)`; // 根据当前索引移动轮播图
-  }, 5000);  // 每 5 秒切换一次
+    currentIndex = (currentIndex + 1) % totalSlides;
+    heroSlides.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }, 5000);
 }
 
 /* EmailJS */
